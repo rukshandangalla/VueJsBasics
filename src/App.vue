f097a120a488d24f9833deae5caf82fb4a577e92
Rukshan Dangalla <rukshandangalla@gmail.com>
2017-06-18 11:59:15 +0530

    adding new components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <test></test>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Test from './components/test'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Test
   }
 }
 </script>
---
+++ src/components/Test.vue
@@ -0,0 +1,53 @@
+﻿<template>
+    <div class="test">
+        <input type="text" v-model="title"><br/>
+        <h1>{{title}}</h1>
+        <p v-if="showName">{{user.firstName}}</p>
+        <p v-else>Nobody</p>
+        <ul>
+            <li v-for="item in items">{{item.title}}</li>
+        </ul>
+        <br/>
+
+        <button v-on:click="greet('Hi')">Say Greeting</button>
+        <br/>
+
+        <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="pressEnter"><br/>
+    </div>
+</template>
+
+<script>
+  export default {
+    name : 'test',
+    data(){
+        return{
+            title: 'Hello World',
+            user:{
+            firstName: 'John',
+            lastName: 'Doe',
+        },
+        showName: true,
+        items: [
+            {title: 'Item One'},
+            {title: 'Item Two'},
+            {title: 'Item Three'}
+            ]
+        }
+    },
+    methods: {
+        greet: function(message){
+            alert(message);
+        },
+        pressKey: function(e){
+            console.log(e.target.value);
+        },
+        pressEnter: function(e){
+            console.log("Press Enter");
+        }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
